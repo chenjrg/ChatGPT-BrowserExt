5a97c38fa5e8b1eb70385bdef994079f8d28b6aa
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-04-09 16:25:47 +0000

    Fix imports

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -1,1 +1,7 @@
-const DEFAULT_DATA_URL = "https://raw.githubusercontent.com/yoanbernabeu/YoanDevGPT/mai
+const DEFAULT_DATA_URL = "https://raw.githubusercontent.com/yoanbernabeu/YoanDevGPT/main/data.json";
+
+chrome.runtime.onInstalled.addListener(setup);
+chrome.runtime.onStartup.addListener(setup);
+
+function setup() {
+    // clear all context menus entries fir
