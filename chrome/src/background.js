39727a9d9728e0acac40bdf17df88bea0a639949
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-05-19 18:16:32 +0000

    chore(package): update mocha to version 5.0.1

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -50,4 +50,8 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
         } else {
             const index = parseInt(info.menuItemId);
 
-            
+            fetch(DATA_URL)
+                .then((response) => response.json())
+                .then((data) => {
+                    const prompt = data[index].prompt;
+                    chrome.scr
