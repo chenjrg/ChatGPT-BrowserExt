4ec251898ebaec219eed54baaf452076ebdf971b
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-07-09 13:39:56 +0000

    Update TestFilters2D.js

--- chrome/src/popup.js
+++ chrome/src/popup.js
@@ -1,1 +1,4 @@
-document.addEventListene
+document.addEventListener('DOMContentLoaded', function () {
+    const dataUrlElement = document.getElementById('data-url');
+    const optionsForm = document.getElementById('options-form');
+   
