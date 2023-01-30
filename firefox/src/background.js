e4d59353b46d32c334e438a1fba07f3735f9dd1d
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-01-30 06:47:02 +0000

    :arrow_up: 1.2.4 :tada:

--- firefox/src/background.js
+++ firefox/src/background.js
@@ -1,4 +1,9 @@
 const DEFAULT_DATA_URL = "https://raw.githubusercontent.com/yoanbernabeu/YoanDevGPT/main/data.json";
 
 browser.runtime.onInstalled.addListener(setup);
-browser.runtime.onStartup.addListener(
+browser.runtime.onStartup.addListener(setup);
+
+function setup() {
+    // clear all context menus entries firstly
+    browser.contextMenus.removeAll().then(() => {
+        browser.storage.local.get('data
